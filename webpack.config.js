0c448035692c76cad15cbf19678ec65cf150b142
Racer <reaganjoseph26@yahoo.com>
2021-03-10 23:58:34 -0500

    images optimize with file loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,31 @@ module.exports = {
         path: __dirname + '/dist'
         
       },
+      module: {
+        //identifes the type of files to pre-process using the test property to find a regular expression
+        //trying to process any image file with the file extension of .jpg
+        rules: [
+          {
+            test: /\.jpg$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name (file) {
+                    return "[path] [name].[ext]"
+                  },
+                  publicPath: function(url) {
+                    return url.replace("../", "/assets/")
+                  }
+                }
+              },
+              {
+                loader: 'image-webpack-loader'
+              }
+            ]
+          }
+        ]
+      },
       //directs webpack what to do. For example letting it know to use jQuery package
       plugins: [
         new webpack.ProvidePlugin({
